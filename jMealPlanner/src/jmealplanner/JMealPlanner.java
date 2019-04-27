4e5341581cae776708049e285b670466832beef2
devinrmiller <drmiller212@gmail.com>
2019-04-27 13:49:50 -0400

    Added statement to main

--- jMealPlanner/src/jmealplanner/JMealPlanner.java
+++ jMealPlanner/src/jmealplanner/JMealPlanner.java
@@ -15,7 +15,8 @@ public class JMealPlanner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FrontEnd fe = new FrontEnd();
+        fe.setVisible(true);
     }
     
 }
